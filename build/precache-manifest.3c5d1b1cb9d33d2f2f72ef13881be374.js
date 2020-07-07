1119b1445e1a38a248e4e95a9159c3daa435f361
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-07-07 17:21:31 +0300

    build v01

---
+++ build/precache-manifest.3c5d1b1cb9d33d2f2f72ef13881be374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca52006f04903098ae6883fdfd63184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ccea795cce39dcb458",
+    "url": "/static/css/main.14f67806.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ccea795cce39dcb458",
+    "url": "/static/js/main.d410e8a5.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
