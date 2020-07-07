5b96d404cb06af973983e8950eaeb170a6f6acf9
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-07-07 17:43:00 +0300

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+// comment for testing build pipeline
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
